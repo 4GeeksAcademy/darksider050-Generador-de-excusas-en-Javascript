6abeb46181a64a1cd133344752ebb6bd59a91326
darksider050 <robertelias38@gmail.com>
2026-05-26 17:51:45 +0000

    generador de excusas de html con javascript completado en funcionamineto, falta css

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
+/* import "bootstrap";
 import "./style.css";
 
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+ */
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generador_de_excusas(who, action, what, when){
+    ramdomwho = Math.floor(Math.random() * who.length);
+    ramdomacction = Math.floor(Math.random() * action.length);
+    ramdomwhat = Math.floor(Math.random() * what.length);
+    ramdomwhen = Math.floor(Math.random() * when.length);
+
+    return who[ramdomwho] + " " + action[ramdomacction] + " " + what[ramdomwhat] + " " + when[ramdomwhen] + ".";
+}
+
+
+function onLoad(){
+    var excuse = document.getElementById('excusa');
+    excuse.innerHTML = generador_de_excusas(who, action, what, when);
+}
+window.onload = onLoad; 
